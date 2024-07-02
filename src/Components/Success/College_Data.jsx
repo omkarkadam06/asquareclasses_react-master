0420c88e5fb160075faf990dc9e689f3283918c0
Amit Kumar <amitkumarr260@gmail.com>
2024-07-02 23:50:58 +0530

    Notification Update

--- src/Components/Success/College_Data.jsx
+++ src/Components/Success/College_Data.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const College_Data = ["IIT Dhanbad", "IIT Dharwad", "COEP Pune", "IISER Pune"];
+const College_Data = ["IIT Gandhinagar", "IIT(ISM) Dhanbad", "IIT Dharwad", "IISER Pune", "IIT Dharwad", "IISER Bhopal", "IIIT Banglore", "IIIT Surat", "IIIT Nagpur", "IIIT Allahabad", "IIIT Pune", "NIT Surat", "NIT Nagpur", "NIT Delhi", "COEP Pune", "VJTI Mumbai", "PICT Pune", "VIT Pune", "VIT Valore", "NDA"];
 
 export default College_Data;
